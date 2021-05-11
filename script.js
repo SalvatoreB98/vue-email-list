7f7c3c257bb80f82d2a321246dba8b485efaa36e
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-05-11 23:21:38 +0200

    Perfezionamenti vari

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 const vue = new Vue({
     el: "#vueContainer",
     data: {
-        mailListCompleted:[],
-        mailList : [],
+        mailListCompleted: [],
+        mailList: [],
+        isLoading: false
     },
-    methods:{
-        randomEmails(){
-            
+    methods: {
+        randomEmails() {
+            this.isLoading = true;
+            this.mailListCompleted = [];
+            for (let i = 0; i < 10; i++) {
+                setTimeout(() => {
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                        .then((resp) => {
+                            let randomMail = resp.data.response;
+                            this.mailList.push(randomMail);
+                            if (this.mailList.length == 10) {
+                                this.mailListCompleted = [...this.mailList]
+                                this.isLoading = false
+                            }
+                        })
+                }, Math.floor(Math.random()*1000))
+            }
         }
     },
-    mounted(){
-        for(i = 0; i< 10; i++){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((resp)=>{
-                let randomMail = resp.data.response;
-                this.mailList.push(randomMail);
-                if(this.mailList.length == 10){
-                    this.mailListCompleted = [...this.mailList]
-                }
-            })
-
-        }
+    mounted() {
+        this.randomEmails();
     }
 });
